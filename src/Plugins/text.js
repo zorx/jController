231d0da6125231a3f72e990f0ff531b1eb0b644f
Sacha WILT <wsacha67@gmail.com>
2014-06-28 00:55:01 +0200

    text plugin file

---
+++ src/Plugins/text.js
@@ -0,0 +1,33 @@
+
+// Text plugin (label, etc)
+$.jController.registerPlugin({
+	name : "text",
+
+	construct : function(params) {
+		var defaults = {
+			x : 0,
+			y : 0,
+			text : "",
+		}
+
+		return $.extend({}, defaults, params);
+	},
+
+	render : function(self) {
+		var params = self.params;
+		$.jController.getHelper("contextDrawText")({
+			ctx    : $.jController.getContext(),
+			x : params.x,
+			y : params.y,
+			baseline : params.baseline,
+			align   : params.align,
+			text    : params.text,
+			font    : params.font,
+			color   : params.color,
+			outline : params.outline,
+			line    : params.line,
+			shadow  : params.shadow,
+			text    : params.text,
+		});
+	},
+});
