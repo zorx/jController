a2ac78e702d29f284dd4cc6593d896215b4a3968
Sacha WILT <wsacha67@gmail.com>
2014-07-03 13:09:35 +0200

    updated mosaic

--- plugins/mosaic.js
+++ plugins/mosaic.js
@@ -16,32 +16,30 @@ $.jController.registerPlugin({
 	},
 
 	path : function (self) {
-		// @TODO
+		(self.getInternal('innerCircle').getPath())();
 	},
 
 	render : function(self) {
-		
-		var ctx = $.jController.getContext();
-		ctx.save();
+
 		self.attr.t = new Date().getTime() * 0.002;
 		self.attr.x = Math.sin(self.attr.t)       * 192 + 256;
 		self.attr.y = Math.cos(self.attr.t * 0.9) * 192 + 256;
-		self.attr.c = 1;//Math.floor(Math.random() * self.attr.colors.length);
+		self.attr.c = Math.floor(Math.random() * self.attr.colors.length);
 
 		if (self.attr.trace) {
 			$.jController.clearCanvas(false);
 		}
-		//console.log(self);
-		var t = self.render("circle", {
+
+		var circle = self.render("circle", {
 			x: self.attr.x,
 			y: self.attr.y,
 			r: self.attr.r,
 			color: self.attr.colors[self.attr.c],
 			fill:  self.attr.colors[self.attr.c],
 		});
-		ctx.restore();
-		//console.log(t);
-		
+
+		self.setInternal({innerCircle: circle})
+
 	},
 
 	events : {
@@ -54,13 +52,10 @@ $.jController.registerPlugin({
 
 				var canvas = $.jController.getCanvas();
 
-				if ($.jController.getHelper("inCircle")({
-					px : data.pageX - canvas.offsetLeft,
-					py : data.pageY - canvas.offsetTop,
-					cx : self.attr.x,
-					cy : self.attr.y,
-					cr : self.attr.r,
-				})) {
+				if (self.inPath(
+					data.pageX - canvas.offsetLeft,
+					data.pageY - canvas.offsetTop
+				)) {
 					callback(self, data);
 				}
 			}
