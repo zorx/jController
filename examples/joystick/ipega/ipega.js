3722d7c46ce41d4ce5724012e49268b1f3762bfe
Sacha WILT <wsacha67@gmail.com>
2014-07-03 13:25:16 +0200

    + ipega

---
+++ examples/joystick/ipega/ipega.js
@@ -0,0 +1,18 @@
+
+$(function() {
+
+	/* Ipega components */
+
+	/* Run Ipega controller */
+	$("#jController").jController({
+		attr: {
+			width: 400,
+			height: 200,
+		}
+
+		events : {
+			// @TODO
+		}
+	})
+
+});
