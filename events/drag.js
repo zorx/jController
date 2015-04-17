654c903e1bca323846abbbb75a68b94a02468512
Amine OUDJEHIH <oudjehih@zedx.org>
2015-04-17 14:17:09 +0200

    refactor(events/drag): indentation

--- events/drag.js
+++ events/drag.js
@@ -1,49 +1,47 @@
 // drag Event
 $.jController.registerEvent({
 
-	name : "drag",
+    name: "drag",
 
-	listener : ["pointerdown","pointerup","mousemove"],
+    listener: ["pointerdown", "pointerup", "mousemove"],
 
-	fn : function (self, callback, data) {
-		
-		var canvas = $.jController.getCanvas();
-		
-		if (data.type == "pointerdown") {
-			if (self.inPath(
-				data.pageX - canvas.offsetLeft,
-				data.pageY - canvas.offsetTop
-			)) {
+    fn: function(self, callback, data) {
 
-				self.setInternal({
-					__draggable : {
-							x : data.pageX,
-							y : data.pageY
-						}
-				});
-			}
-		}
+        var canvas = $.jController.getCanvas();
 
-		else if (data.type == "pointerup") {
+        if (data.type == "pointerdown") {
+            if (self.inPath(
+                data.pageX - canvas.offsetLeft,
+                data.pageY - canvas.offsetTop
+            )) {
 
-			self.setInternal({__draggable:false});
-		}
+                self.setInternal({
+                    __draggable: {
+                        x: data.pageX,
+                        y: data.pageY
+                    }
+                });
+            }
+        } else if (data.type == "pointerup") {
 
-		else if (data.type == "mousemove") {
+            self.setInternal({
+                __draggable: false
+            });
+        } else if (data.type == "mousemove") {
 
-			var draggable = self.getInternal("__draggable");
+            var draggable = self.getInternal("__draggable");
 
-			if (draggable) {
-				self.setInternal({
-					__draggable : {
-							x : data.pageX,
-							y : data.pageY
-						}
-				});
-				self.attr.x = self.attr.x + (data.pageX - draggable.x)
-				self.attr.y = self.attr.y + (data.pageY - draggable.y)
-				callback(self,data);
-			}
-		}
-	}
+            if (draggable) {
+                self.setInternal({
+                    __draggable: {
+                        x: data.pageX,
+                        y: data.pageY
+                    }
+                });
+                self.attr.x = self.attr.x + (data.pageX - draggable.x)
+                self.attr.y = self.attr.y + (data.pageY - draggable.y)
+                callback(self, data);
+            }
+        }
+    }
 });
