cca7000dcba32c311fbc2351040364c91aaff42a
Amine OUDJEHIH <oudjehih@zedx.org>
2015-04-17 14:15:30 +0200

    feat(plugins): remove events

--- plugins/mosaic.js
+++ plugins/mosaic.js
@@ -1,64 +1,43 @@
-
 // Circle plugin (based on Arc)
 $.jController.registerPlugin({
-	
-	name : "mosaic",
 
-	construct : function(params)
-	{
-		var defaults = {
-			r : 0,
-			trace : false,
-			colors : ["rgb(0,0,255)","rgb(0,255,0)","rgb(255,0,0)"]
-		}
+    name: "mosaic",
 
-		return $.extend({}, defaults, params);
-	},
+    construct: function(params) {
+        var defaults = {
+            r: 0,
+            trace: false,
+            colors: ["rgb(0,0,255)", "rgb(0,255,0)", "rgb(255,0,0)"]
+        }
 
-	path : function (self) {
-		(self.getInternal('innerCircle').getPath())();
-	},
+        return $.extend({}, defaults, params);
+    },
 
-	render : function(self) {
+    path: function(self) {
+        (self.getInternal('innerCircle').getPath())();
+    },
 
-		self.attr.t = new Date().getTime() * 0.002;
-		self.attr.x = Math.sin(self.attr.t)       * 192 + 256;
-		self.attr.y = Math.cos(self.attr.t * 0.9) * 192 + 256;
-		self.attr.c = Math.floor(Math.random() * self.attr.colors.length);
+    render: function(self) {
 
-		if (self.attr.trace) {
-			$.jController.clearCanvas(false);
-		}
+        self.attr.t = new Date().getTime() * 0.002;
+        self.attr.x = Math.sin(self.attr.t) * 192 + 256;
+        self.attr.y = Math.cos(self.attr.t * 0.9) * 192 + 256;
+        self.attr.c = Math.floor(Math.random() * self.attr.colors.length);
 
-		var circle = self.render("circle", {
-			x: self.attr.x,
-			y: self.attr.y,
-			r: self.attr.r,
-			color: self.attr.colors[self.attr.c],
-			fill:  self.attr.colors[self.attr.c],
-		});
+        if (self.attr.trace) {
+            $.jController.clearCanvas(false);
+        }
 
-		self.setInternal({innerCircle: circle})
+        var circle = self.render("circle", {
+            x: self.attr.x,
+            y: self.attr.y,
+            r: self.attr.r,
+            color: self.attr.colors[self.attr.c],
+            fill: self.attr.colors[self.attr.c],
+        });
 
-	},
-
-	events : {
-
-		click : {
-
-			listener : "click",
-
-			fn : function (self, callback, data) {
-
-				var canvas = $.jController.getCanvas();
-
-				if (self.inPath(
-					data.pageX - canvas.offsetLeft,
-					data.pageY - canvas.offsetTop
-				)) {
-					callback(self, data);
-				}
-			}
-		}
-	}
+        self.setInternal({
+            innerCircle: circle
+        })
+    },
 })
